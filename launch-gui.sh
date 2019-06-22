af582bba7e94d3b10bb197b90fb3d87fedf753c3
chantzish <chantzish@gmail.com>
2019-06-22 04:05:47 +0300

    auto launch eclipse

--- launch-gui.sh
+++ launch-gui.sh
@@ -3,8 +3,12 @@
 export HOME=/home/user
 export USER=`whoami`
 export LANG=en_IL
+export JAVA_HOME=/opt/jdk1.6.0_45/
 mkdir -m 1777 /tmp/.X11-unix
 Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./0.log -config ./xorg.conf :0 &
 sleep 5s
+DISPLAY=:0 openbox-session &
+sleep 5s
+DISPLAY=:0 eclipse &
 xauth generate :0 . trusted
 DISPLAY=:0 x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth ~/.vnc/passwd -rfbport 5900 -shared &

4043dad400b58e4a0a5e33029b069c8a63fbe615
chantzish <chantzish@gmail.com>
2019-07-23 23:22:20 +0300

    fix postgres user

--- launch-gui.sh
+++ launch-gui.sh
@@ -13,9 +13,10 @@ DISPLAY=:0 lxterminal &
 xauth generate :0 . trusted
 DISPLAY=:0 x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth ~/.vnc/passwd -rfbport 5900 -shared &
 printf "%s\n" "$HEROKU_LOGIN" > .netrc
-pg_createcluster -u `whoami` 10 main
-sed -i "s/install -d -m 2775 -o postgres -g postgres \/var\/run\/postgresql/install -d -m 2775 -o `whoami` -g `id -gn` \/var\/run\/postgresql/" /usr/share/postgresql-common/init.d-functions
+#pg_createcluster -u `whoami` 10 main
+#sed -i "s/install -d -m 2775 -o postgres -g postgres \/var\/run\/postgresql/install -d -m 2775 -o `whoami` -g `id -gn` \/var\/run\/postgresql/" /usr/share/postgresql-common/init.d-functions
+echo "mymap           `whoami`                  postgres" >> /etc/postgresql/10/main/pg_ident.conf
 service postgresql start
-sleep 5s
-createdb
+#sleep 5s
+#createdb
 env > env
